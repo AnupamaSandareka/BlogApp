63859b82a5c503b689c35dcf8bf0bb5ab0222fbc
AnupamaSandareka <gmanupama99@gmail.com>
2024-03-09 15:10:37 +0530

    swagger annotation with open_api

--- src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,36 @@
 package com.springboot.blog;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot Blog App APIs",
+				description = "Spring Boot Blog App APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Anu",
+						email = "anu@gmail.com",
+						url = "https://www.blogapp.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.blogapp.com/license"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot Blog App Documentation",
+				url = "https://github.com/AnupamaSandareka/BlogApp"
+		)
+)
 public class SpringbootBlogRestApiApplication {
 
 	@Bean
